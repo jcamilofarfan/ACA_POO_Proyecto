17988fce7c4d13951967603146652d40e76b6df0
jcamilofarfan <farfan1976@hotmail.com>
2025-03-13 15:56:28 -0500

    Refactor ResponseRatesDto to remove unused properties and simplify JSON structure

--- src/main/java/com/miaca/dto/ResponseRatesDto.java
+++ src/main/java/com/miaca/dto/ResponseRatesDto.java
@@ -5,64 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Map;
 
 public class ResponseRatesDto {
-    @JsonProperty("result")
-    private String result;
-
-    @JsonProperty("documentation")
-    private String documentation;
-
-    @JsonProperty("terms_of_use")
-    private String termsOfUse;
-
-    @JsonProperty("time_last_update_unix")
-    private long timeLastUpdateUnix;
-
-    @JsonProperty("time_last_update_utc")
-    private String timeLastUpdateUtc;
-
-    @JsonProperty("time_next_update_unix")
-    private long timeNextUpdateUnix;
-
-    @JsonProperty("time_next_update_utc")
-    private String timeNextUpdateUtc;
-
-    @JsonProperty("base_code")
-    private String baseCode;
-
     @JsonProperty("conversion_rates")
     private Map<String, Double> conversionRates;
-
-    public String getResult() {
-        return result;
-    }
-
-    public String getDocumentation() {
-        return documentation;
-    }
-
-    public String getTermsOfUse() {
-        return termsOfUse;
-    }
-
-    public long getTimeLastUpdateUnix() {
-        return timeLastUpdateUnix;
-    }
-
-    public String getTimeLastUpdateUtc() {
-        return timeLastUpdateUtc;
-    }
-
-    public long getTimeNextUpdateUnix() {
-        return timeNextUpdateUnix;
-    }
-
-    public String getTimeNextUpdateUtc() {
-        return timeNextUpdateUtc;
-    }
-
-    public String getBaseCode() {
-        return baseCode;
-    }
 
     public Map<String, Double> getConversionRates() {
         return conversionRates;
